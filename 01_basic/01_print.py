32ef11456960fd1e008fcf47c4d8ea9c32b97d5d
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-15 13:48:40 -0400

    first commit

---
+++ 01_basic/01_print.py
@@ -0,0 +1,11 @@
+print("Hello, Twitch!")
+print('Esto tambien funciona con single quote')
+
+print('Python', 'es', 'genial')
+
+print('Python', 'es', 'brutal', sep = '-')
+
+print('Esto se imprime', end = ' ')
+print('en una linea')
+
+
