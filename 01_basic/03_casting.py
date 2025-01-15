3b30005486989a1bc84adf957454a940a876e4dd
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-15 14:16:57 -0400

    Add casting examples to 03_casting.py for type conversion demonstration

---
+++ 01_basic/03_casting.py
@@ -0,0 +1,30 @@
+print('Conversion de tipos')
+"""
+This script demonstrates type conversion (casting) in Python. It includes examples of converting between different data types such as integers, floats, strings, and booleans.
+Examples:
+  - Converting a string to an integer and performing arithmetic operations.
+  - Converting an integer to a string and concatenating it with another string.
+  - Converting a float to an integer and rounding floats to the nearest integer.
+  - Evaluating the boolean value of different integers and strings.
+Note:
+  - The `round()` function rounds to the nearest even number when the value is exactly halfway between two integers.
+  - The `bool()` function returns `False` for zero, empty strings, and `None`, and `True` for all other values.
+"""
+
+# print(type(int('100')))
+# print(int('100') + 2)
+# print('100' + str(2))
+
+# print(float(3.14))
+# print(int(3.14))
+# print(round(3.5)) # Va redondear al par mas cercano
+# print(round(4.5)) 
+# print(round(2.5)) 
+
+# print(bool(3))
+# print(bool(0))
+# print(bool(-1))
+
+# print(bool(''))
+# print(bool(' '))
+# print(bool('False'))
