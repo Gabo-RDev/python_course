92e7374235de2999099cfa3a8a60727e0ea23bd9
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-15 13:51:34 -0400

    Add documentation to print.py demonstrating print function usage

--- 01_basic/01_print.py
+++ 01_basic/01_print.py
@@ -1,4 +1,14 @@
 print("Hello, Twitch!")
+"""
+This script demonstrates various ways to use the print function in Python.
+Functions:
+  print("Hello, Twitch!"): Prints a greeting message.
+  print('Esto tambien funciona con single quote'): Prints a message using single quotes.
+  print('Python', 'es', 'genial'): Prints multiple strings separated by a space.
+  print('Python', 'es', 'brutal', sep = '-'): Prints multiple strings separated by a hyphen.
+  print('Esto se imprime', end = ' '): Prints a message without a newline at the end.
+  print('en una linea'): Prints a message on the same line as the previous print statement.
+"""
 print('Esto tambien funciona con single quote')
 
 print('Python', 'es', 'genial')
