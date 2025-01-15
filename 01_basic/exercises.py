d895bbed69aeb44e5154fb49738fadf71a8a5f41
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-15 15:26:15 -0400

    Add exercises for practicing basic Python concepts in exercises.py

---
+++ 01_basic/exercises.py
@@ -0,0 +1,54 @@
+import math
+
+###
+# exercises.py
+# Ejercicios para practicar los conceptos aprendidos en las lecciones.
+###
+
+print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas.")
+
+### Completa aquí
+
+print('Soy Gabriel\nvivo en Quema Creta')
+
+print("\nEjercicio 2: Muestra los tipos de datos de las siguientes variables:")
+print("Usa el comando 'type()' para determinar el tipo de datos de cada variable.")
+a = 15
+b = 3.14159
+c = "Hola mundo"
+d = True
+e = None
+
+### Completa aquí
+
+print(f'{type(a)}, {type(b)}, {type(c)}, {type(d)}, {type(e)}')
+
+print("\nEjercicio 3: Casting de tipos")
+print("Convierte la cadena \"12345\" a un entero y luego a un float.")
+print("Convierte el float 3.99 a un entero. ¿Qué ocurre?")
+
+### Completa aquí
+
+print(int('12345'), str(12345), int(3.99)) # Ocurre que en vez de redondear, no lo hace y se queda en 3. 
+
+print("\nEjercicio 4: Variables")
+print("Crea variables para tu nombre, edad y altura.")
+print("Usa f-strings para imprimir una presentación.")
+
+# "Hola! Me llamo midudev y tengo 39 años, mido 1.70 metros"
+
+### Completa aquí
+name = 'Gabriel'
+age = 22
+size = 1.80
+
+print(f'Hola! Me llamo {name} y tengo {age} años, mido {size} metros')
+
+print("\nEjercicio 5: Números")
+print("1. Crea una variable con el número PI (sin asignar una variable)")
+print("2. Redondea el número con round()")
+print("3. Haz la división entera entre el número que te salió y el número 2")
+print("4. El resultado debería ser 1")
+
+print(f'{round(math.pi) // 2}')
