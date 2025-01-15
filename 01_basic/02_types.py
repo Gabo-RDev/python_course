2b90f76d95e08b8545b0ce9525ce49d91fac7ac6
ImNotGabo <132179691+ImNotGabo@users.noreply.github.com>
2025-01-15 14:03:46 -0400

    Add example code for basic data types in 02_types.py

---
+++ 01_basic/02_types.py
@@ -0,0 +1,27 @@
+print('int:')
+#print(10)
+#print(0)
+#print(-5)
+#print(type(3920749279437908278402380))
+
+print('float:')
+#print(type(3.14))
+#print(type(1.0))
+#print(type(1e3))
+
+print('complex:')
+# print(type(1 + 2j))
+
+print('str:')
+# print('Hola')
+# print('123')
+# print(""" Multilinea """)
+
+
+print('bool:')
+# print(True)
+# print(False)
+# print(1 < 2)
+
+print('NoneType:')
+# print(None)
